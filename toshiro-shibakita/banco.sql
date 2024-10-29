4a3facbdde4d08c08679e2abcff4ad9c88741e21
danilolimadev <danilolimadev@gmail.com>
2024-10-29 20:34:00 -0300

    alteracao banco de dados

--- toshiro-shibakita/banco.sql
+++ toshiro-shibakita/banco.sql
@@ -1,8 +1,9 @@
-CREATE TABLE dados (
+CREATE TABLE alunos (
     id int,
     nome varchar(50),
     sobrenome varchar(50),
     endereco varchar(150),
     cidade varchar(50),
+    documento varchar(11),
     host varchar(50)
 );
